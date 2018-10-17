9131f40784efb9883b58f64345c82e586e5d2c50
Yana <holoborodkoyana97@gmail.com>
2018-10-17 13:15:22 +0200

    Project files uploaded

---
+++ js/jquery.main.js
@@ -0,0 +1,26 @@
+$('#pricing-b-1').on('mouseover', function() {
+    $('#pricing-b-1').css('background','#ff7200');
+    $('#pricing-s-1').css('color','#ff7200');
+});
+$('#pricing-b-1').on('mouseout', function() {
+    $('#pricing-b-1').css('background','');
+    $('#pricing-s-1').css('color','');
+});
+
+$('#pricing-b-2').on('mouseover', function() {
+    $('#pricing-b-2').css('background','#ff7200');
+    $('#pricing-s-2').css('color','#ff7200');
+});
+$('#pricing-b-2').on('mouseout', function() {
+    $('#pricing-b-2').css('background','');
+    $('#pricing-s-2').css('color','');
+});
+
+$('#pricing-b-3').on('mouseover', function() {
+    $('#pricing-b-3').css('background','#ff7200');
+    $('#pricing-s-3').css('color','#ff7200');
+});
+$('#pricing-b-3').on('mouseout', function() {
+    $('#pricing-b-3').css('background','');
+    $('#pricing-s-3').css('color','');
+});
